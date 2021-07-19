e4841a39c0432b18bbb7d82ec52b024a94146057
ebarra (underworlddemon) <d9067816565@gmail.com>
2021-07-19 17:10:58 +0300

    fix merge1

--- ConfigParser.hpp
+++ ConfigParser.hpp
@@ -3,5 +3,6 @@
 
 //int ParseConfig2(std::string fileName);
 void parseConfig(std::string in_string2, Http_config* http_config);
+void printConfig(Http_config* http_config);
 
 #endif //WEBSERV_21_CONFIGPARSER_HPP
