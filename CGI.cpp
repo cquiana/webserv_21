db031b01fcaab6b9e0bd607ea3e09c21dd6fb0d5
jlyessa <jlyessa@student.21-school.ru>
2021-07-19 16:29:53 +0300

    CGI

--- CGI.cpp
+++ CGI.cpp
@@ -5,6 +5,49 @@ CGI::CGI(std::map<std::string, std::string> headers,
 		 const std::string &cgi_ext)
 		 : _env(NULL), _headers(headers), _cgi_path(cgi_path), _cgi_ext(cgi_ext) {}
 
+const std::string &CGI::startCGI() {
+	const std::string &res;
+
+	for(auto it = headers.begin(); it != headers.end(); ++it) {
+		CGI::pushEnv(it->first, it->second);
+	}
+
+	pid_t pid;
+	int In;
+	int Out;
+	int saveFdIn = dup(0);
+	int saveFdOut = dup(1);
+
+	pid = fork();
+	if (pid == -1) {
+		std::cerr << "Fork crashed." << std::endl;
+	} else if (!pid) {
+		char * const *nul = NULL;
+		dup2(In, 0);
+		dup2(Out, 1);
+		execve(this->cgi_path.c_str(), nul, this->_env);
+	} else {
+		char	buffer[100000] = {0};
+
+		waitpid(-1, NULL, 0);
+
+		int ret = 1;
+		while (ret > 0)
+		{
+			memset(buffer, 0, 100000);
+			ret = read(Out, buffer, 100000 - 1);
+			res += buffer;
+		}
+	}
+	dup2(saveFdIn, 0);
+	dup2(saveFdOut, 1);
+	close(In);
+	close(Out);
+	close(saveFdIn);
+	close(saveFdOut);
+	return res;
+}
+
 CGI::~CGI() {
 	for (int i = 0; this->_env[i]; ++i) {
 		delete[] this->_env[i];
@@ -37,19 +80,4 @@ void CGI::pushEnv(const std::string &key, const std::string &value) {
 		this->_env[0] = strdup(text);
 		this->_env[1] = NULL;
 	}
-}
-
-void CGI::Start() {
-	CGI::pushEnv("Content-Type", "text");
-	CGI::pushEnv("QUERY_STRING", this->_request);
-
-	for (int i = 0; this->_env[i]; ++i) {
-		std::cout << this->_env[i] << std::endl;
-	}
-}
-
-int main() {
-	CGI local("alex=23");
-
-	local.Start();
 }
